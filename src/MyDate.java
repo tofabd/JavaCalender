d79f2151cab92623529859069ef8a65a7b834ac9
tofabd <tofa.du@gmail.com>
2018-08-31 09:09:53 +0600

    Updated

--- src/MyDate.java
+++ src/MyDate.java
@@ -9,6 +9,7 @@ public class MyDate {
         Calendar calendar2 = Calendar.getInstance();
         SimpleDateFormat SDF_MONTH = new SimpleDateFormat("MMM dd", Locale.US);
         SimpleDateFormat SDF_YEAR = new SimpleDateFormat("yyyy", Locale.US);
+        SimpleDateFormat SDF_DAY = new SimpleDateFormat("EEEEE", Locale.US);
 
         //calendar2.add(Calendar.WEEK_OF_MONTH,-1);
         //calendar2.set(Calendar.WEEK_OF_MONTH,-1);
@@ -27,6 +28,7 @@ public class MyDate {
         System.out.println("DAY_OF_WEEK: " + calendar.get(Calendar.DAY_OF_WEEK)+" "+ calendar2.get(Calendar.DAY_OF_WEEK));
         System.out.println("WEEK_OF_MONTH: " + calendar.get(Calendar.WEEK_OF_MONTH)+" "+ calendar2.get(Calendar.WEEK_OF_MONTH));
         System.out.println("WEEK_OF_YEAR: " + calendar.get(Calendar.WEEK_OF_YEAR)+" "+ calendar2.get(Calendar.WEEK_OF_YEAR));
+        System.out.println("*********************************************************************");
 
         //To get Actual Week of The Year
         int day_of_week=calendar2.get(Calendar.DAY_OF_WEEK);
@@ -38,14 +40,21 @@ public class MyDate {
 
         //Another Technique
         calendar2.add(Calendar.DATE,-1);
+        calendar2.set(Calendar.DAY_OF_WEEK,Calendar.SUNDAY);
         System.out.println("WEEK_OF_YEAR: " + calendar.get(Calendar.WEEK_OF_YEAR)+" "+ calendar2.get(Calendar.WEEK_OF_YEAR));
+        System.out.println("Month: " + calendar.get(Calendar.MONTH)+" "+ calendar2.get(Calendar.MONTH)); // jan =0, dec=11
+        System.out.println("Date: " + calendar.get(Calendar.DATE)+" "+ calendar2.get(Calendar.DATE));
+        System.out.println("DAY: " + SDF_DAY.format(calendar.getTime())+" "+ SDF_DAY.format(calendar2.getTime()));
+
+        System.out.println("*********************************************************************");
+
 
         System.out.println("Hour: " + calendar.get(Calendar.HOUR_OF_DAY)+" "+ calendar2.get(Calendar.HOUR_OF_DAY));
         System.out.println("Minute: " + calendar.get(Calendar.MINUTE)+" "+ calendar2.get(Calendar.MINUTE));
         System.out.println("Second: " + calendar.get(Calendar.SECOND)+" "+ calendar2.get(Calendar.SECOND));
         System.out.println("MilliSecond: " + calendar.get(Calendar.MILLISECOND)+" "+ calendar2.get(Calendar.MILLISECOND));
 
-        System.out.println("MilliSecond:  " + calendar.getTimeInMillis()+" - "+calendar2.getTimeInMillis()+" = "+(calendar.getTimeInMillis()-calendar2.getTimeInMillis()));
+        System.out.println("MilliSecond Diff " + calendar.getTimeInMillis()+" - "+calendar2.getTimeInMillis()+" = "+(calendar.getTimeInMillis()-calendar2.getTimeInMillis()));
 
         System.out.println("Day Start:  " +calendar2.getTimeInMillis());
         calendar2.add(Calendar.DATE, -1);
